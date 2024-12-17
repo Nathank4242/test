377803f35ed9f7541e2f41a91b269e93e12c20f2
Nathank4242 <nkweon@gmail.com>
2024-12-17 14:01:09 -0500

    test1

--- V24E.js
+++ V24E.js
@@ -23011,4 +23011,13 @@ let data = [
 ];
 //1 print each card where the series set is "Diamond & Pearl"
 
+const filtered = data.filter((series) => (data.series = "Diamond & Pearl"));
+console.log(filtered);
+
 //2 print each card name where the card type "includes" Water
+
+// const res = data.includes((data.types = "water"));
+
+// if (res) {
+//   console.log(res);
+// }
